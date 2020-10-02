798e70d6e758fb8fce4247f75e1ff9ba5fbeefdb
Mohammed El-Afifi <MElAfifi@luxoft.com>
2020-10-02 11:23:10 +0200

    Use rstrip instead of splitlines

--- src/makeRecipe.py
+++ src/makeRecipe.py
@@ -139,7 +139,7 @@ def run(rpm_list_file, settings):
     with open(repo_file) as repo_map_file:
         for line in repo_map_file:
 
-            line = line.splitlines()[0]
+            line = line.rstrip()
 
             if line:  # Overlook empty lines.
 
@@ -168,8 +168,7 @@ def run(rpm_list_file, settings):
     with open(rpm_list_file) as rpm_list:
         for rpm_info in rpm_list:
 
-            # Remove end-of-line characters.
-            rpm_info = rpm_info.splitlines()[0]
+            rpm_info = rpm_info.rstrip()  # Remove end-of-line characters.
 
             if rpm_info:  # Overlook empty lines.
 
